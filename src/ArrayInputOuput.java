ee20d4995b902329bd8a2e81c5e01394b0c1e7a9
Youngju KIM <fjvbn2003@gmail.com>
2021-10-16 01:11:45 +0900

    Initial commit

---
+++ src/ArrayInputOuput.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ArrayInputOuput {
+
+    static int N;
+    static int[] Score = new int[100];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        for( int i=0; i<N; i++){
+            Score[i] = sc.nextInt();
+        }
+        for(int i=0; i<N; i++){
+            System.out.print(Score[i]+" ");
+        }
+
+    }
+}
