1f0be867c8413bcdf9e47a7d31743fe13e343a6c
Youngju KIM <fjvbn2003@gmail.com>
2021-10-29 21:53:21 +0900

    Solve BOJ1477

---
+++ src/BFS.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class BFS {
+    public static void main(String[] args) {
+        int[][] arr  = {{1,4,2},{4,2,1},{2,3,4}};
+
+        Arrays.sort(arr, (o1, o2)-> o1[0]-o2[0]);
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[i].length; j++){
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
